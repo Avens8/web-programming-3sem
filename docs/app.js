d1bb45c2d12afb5d9ce9724cb1f1db30f14e6c9a
Avens8 <tsayipad@gmail.com>
2017-11-22 01:04:12 +0300

    changed background

--- docs/app.js
+++ docs/app.js
@@ -2,12 +2,12 @@ function addText (txt) {
     var elem = $('<li><span></span><button class="button_rm">Удалить</button></li>');
     $('span', elem).text(txt);
     $('#root ul').append(elem);
-    $('.button_rm', elem).click(function (e) {$(this).parent().remove()});
+    $('.button_rm', elem).click(function () {$(this).parent().remove()});
 }
 
-$(function () {
+$(document).ready(function () {
     $('#root').append('<ul></ul>');
-    $('#root').append('<input id="add_task_input"><button id="add_task">Добавить</button>');
+    $('#root').append('<input id="add_task_input"><button id="add_task" class="input_button">Добавить</button>');
     $('#add_task').click(function() {addText($('#add_task_input').val())});
     addText('Сделать задание #3 по web-программированию');
 })
