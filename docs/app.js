1f3d3e92521af05bdf9eb63a90f92e4779a6427e
Avens8 <tsayipad@gmail.com>
2017-11-22 16:44:44 +0300

    final version, changed app.js

--- docs/app.js
+++ docs/app.js
@@ -1,13 +1,25 @@
 function addText (txt) {
-    var elem = $('<li><span></span><button class="button_rm">Удалить</button></li>');
-    $('span', elem).text(txt);
-    $('#root ul').append(elem);
-    $('.button_rm', elem).click(function () {$(this).parent().remove()});
+    if (txt != '') {
+        var elem = $('<li><span></span><button class="button_rm">Удалить</button></li>');
+        $('span', elem).text(txt);
+        $('#root ul').append(elem);
+        $('.button_rm', elem).click(function () {$(this).parent().remove()});
+    }
 }
 
 $(document).ready(function () {
     $('#root').append('<ul></ul>');
     $('#root').append('<input id="add_task_input"><button id="add_task" class="input_button">Добавить</button>');
-    $('#add_task').click(function() {addText($('#add_task_input').val())});
+    $('#add_task').click(function() {
+        addText($('#add_task_input').val());
+        $(this).parent().find('#add_task_input').val('');
+});
+    $('#add_task_input').keypress(function (e) {
+        if (e.which == 13) {
+            e.preventDefault();
+            addText($('#add_task_input').val());
+            $(this).val('');
+        }
+    })
     addText('Сделать задание #3 по web-программированию');
 })
